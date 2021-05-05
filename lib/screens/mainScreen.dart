1ff8a70ecb9dee16b68cffe496f9fa300a3e2526
shehan82 <shehansandeepa82@gmail.com>
2021-05-05 08:23:50 +0530

    added homeScreen, drawerScreen, MainScreen

---
+++ lib/screens/mainScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_with_drawer/screens/drawer.dart';
+import 'package:flutter_ui_with_drawer/screens/home.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [DrawerScreen()],
+    );
+  }
+}

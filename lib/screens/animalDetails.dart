97a25cfd8e5ee13bdf131181f270f0921c7e8b43
shehan82 <shehansandeepa82@gmail.com>
2021-05-06 21:42:13 +0530

    add middle box in the animalDetails screen

--- lib/screens/animalDetails.dart
+++ lib/screens/animalDetails.dart
@@ -11,20 +11,61 @@ class _AnimalDetailsState extends State<AnimalDetails> {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
-            height: (MediaQuery.of(context).size.height / 2) + 50,
-            color: Color(0xffc4d4d4),
+          Positioned(
+            child: Container(
+              // height: (MediaQuery.of(context).size.height / 2) + 50,
+              color: Colors.white,
+            ),
+          ),
+          Positioned(
+            child: Container(
+              height: (MediaQuery.of(context).size.height / 2) + 50,
+              color: Color(0xffc4d4d4),
+            ),
           ),
           Positioned(
               width: 200,
               left: (MediaQuery.of(context).size.width / 2) - 100,
-              top: 60,
+              top: 80,
               child: Container(
                 child: Container(
-                  color: Colors.grey,
+                  // color: Colors.grey,
                   child: Image.asset(
                     'assets/images/pet-cat1.png',
                   ),
+                ),
+              )),
+          Positioned(
+              width: MediaQuery.of(context).size.width,
+              top: 60,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                // color: Colors.grey,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.arrow_back,
+                      size: 30,
+                      color: Colors.grey[800],
+                    ),
+                    Icon(
+                      Icons.cloud_upload_outlined,
+                      size: 30,
+                      color: Colors.grey[800],
+                    )
+                  ],
+                ),
+              )),
+          Positioned(
+              top: (MediaQuery.of(context).size.height / 2) - 25,
+              child: Container(
+                height: 150,
+                width: MediaQuery.of(context).size.width - 40,
+                margin: EdgeInsets.only(left: 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  color: Colors.black,
                 ),
               ))
         ],
